a956c558b3edf4e32c04a3d3e5eb88d63862c950
Jason Nicholas <jasonOey07@gmail.com>
2025-07-10 18:28:09 +0700

    Create assignMultipleVar.py

---
+++ VARIABLES/assignMultipleVar.py
@@ -0,0 +1,10 @@
+#assign multiple variables
+
+hi = "Hello"
+name = "Bro"
+age, height, city = 38, 175, "Mexico"
+
+print(hi +", "+ name+"!")          # print output
+print("You're",age, "Years Old!")
+print(f"You're {height} cm.")
+print(f"You're from {city}.")
